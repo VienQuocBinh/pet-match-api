720a0d56befe33698cbb63d5ebe0530ac1bf8d3e
noelvu <thuyenvuanh2412@gmail.com>
2023-05-16 05:45:49 +0700

    update Profile model and add db script

---
+++ src/main/resources/db/migration/V2__insert_user.sql
@@ -0,0 +1,9 @@
+INSERT INTO public."user" (id, created_ts, email, password, phone, updated_ts) VALUES ('U4Gnn6koanStHJUKhMmYJBh23Tq1', '2023-05-16 05:22:06.000000', 'thuyenvuanh2412@gmail.com', '123456789', '0795870939', '2023-05-16 05:22:55.000000');
+INSERT INTO public."species" (id, name) values ('d5652886-f36f-11ed-a05b-0242ac120003', 'Cho');
+INSERT INTO public."species" (id, name) values ('d5652b1a-f36f-11ed-a05b-0242ac120003', 'Meo');
+INSERT INTO public."species" (id, name) values ('d5652c64-f36f-11ed-a05b-0242ac120003', 'Ca');
+INSERT INTO public."species" (id, name) values ('d5652d9a-f36f-11ed-a05b-0242ac120003', 'Chim');
+INSERT INTO public."breed" (id, name, species_id) values ('f152bcf2-f36f-11ed-a05b-0242ac120003', 'Cho Husky', 'd5652886-f36f-11ed-a05b-0242ac120003');
+INSERT INTO public."breed" (id, name, species_id) values ('f152bfae-f36f-11ed-a05b-0242ac120003', 'Cho Alaska', 'd5652886-f36f-11ed-a05b-0242ac120003');
+INSERT INTO public."breed" (id, name, species_id) values ('f152c102-f36f-11ed-a05b-0242ac120003', 'Shiba Inu', 'd5652886-f36f-11ed-a05b-0242ac120003');
+INSERT INTO public."breed" (id, name, species_id) values ('f152c256-f36f-11ed-a05b-0242ac120003', 'Cho Poodle', 'd5652886-f36f-11ed-a05b-0242ac120003');
