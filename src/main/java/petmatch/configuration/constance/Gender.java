d7fb6791acbf9626794b71075d9e12cb6ca48c5d
VienQuocBinh <91947000+VienQuocBinh@users.noreply.github.com>
2023-05-15 21:36:08 +0700

    Create db script

---
+++ src/main/java/petmatch/configuration/constance/Gender.java
@@ -0,0 +1,7 @@
+package petmatch.configuration.constance;
+
+public enum Gender {
+    MALE,
+    FEMALE,
+    OTHER
+}
--- src/main/java/petmatch/model/Profile.java
+++ src/main/java/petmatch/model/Profile.java
@@ -5,6 +5,7 @@ import jakarta.validation.constraints.NotNull;
 import lombok.*;
 import org.hibernate.annotations.GenericGenerator;
 import org.hibernate.validator.constraints.Length;
+import petmatch.configuration.constance.Gender;
 
 import java.util.Date;
 import java.util.List;
@@ -25,7 +26,8 @@ public class Profile {
     @Length(min = 3, max = 100)
     private String name;
     private Date birthDate;
-    private String gender; // enum gender
+    @Enumerated(EnumType.STRING)
+    private Gender gender; // enum gender
     private String description;
     private String attribute;
     @ElementCollection
